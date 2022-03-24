0eecb2a87c9b88d658333b7461ab59b58ce67084
Shoya-cyber <shoyas0627@gmail.com>
2022-03-24 12:25:07 +0900

    product store

--- app/app/Models/Stock.php
+++ app/app/Models/Stock.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Intervention\Image\Gd\Commands\FillCommand;
 
 class Stock extends Model
 {
     use HasFactory;
 
     protected $table = 't_stocks';
+
+    protected $fillable =[
+        'product_id',
+        'type',
+        'quantity'
+    ];
 }
